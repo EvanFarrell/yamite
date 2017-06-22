04ec5798d995293351bb737ac0d6adaee6a80726
Evan Farrell <evan.farrell0@gmail.com>
2017-06-22 09:48:36 -0400

    basic logic for parsing dicts

--- parser.py
+++ parser.py
@@ -1,2 +1,16 @@
 import yaml
 
+with open("test.yaml", 'r') as stream:
+    try:
+        parsed_yaml = yaml.load(stream)
+        print parsed_yaml
+
+        for person in parsed_yaml:
+            print person
+
+            for trait in parsed_yaml[person]:
+                print trait
+
+    except yaml.YAMLError as exc:
+        print exc
+
